adb48212f54c72b78491dc9ae6ddb5a54bd46d03
Benjamin Costanzo <benjamincostanzo23@gmail.com>
2023-03-29 14:48:10 -0300

    Guia4 ejercicio 1 hecho

--- Guia4Subprogramas/src/guia4subprogramas/Guia4Subprogramas_Ejercicio1.java
+++ Guia4Subprogramas/src/guia4subprogramas/Guia4Subprogramas_Ejercicio1.java
@@ -5,6 +5,8 @@
  */
 package guia4subprogramas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Costanzo
@@ -15,7 +17,48 @@ public class Guia4Subprogramas_Ejercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        Scanner leer = new Scanner(System.in);
+        float a,b,resultado;
+        System.out.println("Ingrese 2 números: ");
+        a = leer.nextFloat();
+        b = leer.nextFloat();
+        
+        System.out.println("Seleccione una operación: ");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicación");
+        System.out.println("4. División");
+
+        switch (leer.nextInt()) {
+            case 1:
+                System.out.println(sumar(a,b));
+                break;
+            case 2:
+                System.out.println(restar(a,b));
+                break;
+            case 3:
+                System.out.println(multiplicar(a,b));               
+                break;
+            case 4:
+                System.out.println(dividir(a,b));               
+                break;
+        }
+    }   
     
+    public static float sumar(float a,float b){
+
+        return a+b;
+    }
+    public static float restar(float a,float b){
+
+        return a-b;
+    }
+    public static float multiplicar(float a,float b){
+
+        return a*b;
+    }
+    public static float dividir(float a,float b){
+
+        return a/b;
+    }
 }
