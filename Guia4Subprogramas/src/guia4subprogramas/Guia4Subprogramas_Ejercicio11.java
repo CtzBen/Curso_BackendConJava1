ffeabfde3e967f6bd3dbe90eb7af3a94a03cf967
Benjamin Costanzo <benjamincostanzo23@gmail.com>
2023-03-29 10:56:44 -0300

    IT WORKS!

--- Guia4Subprogramas/src/guia4subprogramas/Guia4Subprogramas_Ejercicio11.java
+++ Guia4Subprogramas/src/guia4subprogramas/Guia4Subprogramas_Ejercicio11.java
@@ -5,17 +5,73 @@
  */
 package guia4subprogramas;
 
+import java.util.Scanner;
 /**
  *
  * @author Costanzo
+ * Escribir un programa que procese una secuencia de caracteres ingresada por teclado y terminada en punto,
+ * y luego codifique la palabra o frase ingresada de la siguiente manera:
+ * Cada vocal se reemplaza por el carácter que se indica en la tabla
+ * y el resto de los caracteres (incluyendo a las vocales acentuadas) se mantienen sin cambios.
+Realice un subprograma que reciba una secuencia de caracteres y retorne la codificación correspondiente.
+* Utilice la estructura “según” para la transformación. 
+Por ejemplo, si el usuario ingresa:     Ayer, lunes, salimos a las once y 10.
+La salida del programa debería ser:  @y#r, l*n#s, s@l$m%s @ l@s %nc# y 10.
+
  */
-public class Guia4Subprogramas {
+public class Guia4Subprogramas_Ejercicio11 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        String secuencia;
+        
+        System.out.println("Ingrese una secuencia de caracteres, ya sea una frase o letras aleatorias: ");
+        
+        secuencia = leer.nextLine();
+        
+        secuencia = codificar(secuencia);
+        System.out.println(secuencia);
     }
     
+
+public static String codificar(String secuencia){
+
+    String resultado = secuencia;
+    
+    for (int i = 0; i < secuencia.length() ; i++){
+        switch (secuencia.substring(i,i+1)){
+            case "a":
+            case "A":   
+                resultado = resultado.substring(0, i).concat("@" + resultado.substring(i+1, resultado.length()));
+                break;
+            case "e":
+            case "E":
+                resultado = resultado.substring(0, i).concat("#" + resultado.substring(i+1, resultado.length()));
+                break;
+            case "i":
+            case "I":
+                resultado = resultado.substring(0, i).concat("$" + resultado.substring(i+1, resultado.length()));
+                break;
+            case "o":
+            case "O":
+                resultado = resultado.substring(0, i).concat("%" + resultado.substring(i+1, resultado.length()));
+                break;
+            case "u":
+            case "U":
+                resultado = resultado.substring(0, i).concat("*" + resultado.substring(i+1, resultado.length()));
+                break;    
+        }
+    }
+
+
+    
+    
+    
+
+    return resultado;
+}
+
 }
